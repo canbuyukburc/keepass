0a5ed95732bacadcffd890fb2f1ec430b8943f23
Can Buyukburc <canbuyukburc@hotmail.com>
2022-07-27 22:22:15 +0200

    latest

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ echo $pfad > init.ini
 
 echo "please enter Keepass DB password"
 read keepasspwd
-echo "${keepasspwd}" | openssl aes-256-cbc -a -pass pass:$tuz > ~/.ssh/salted
+#echo "${keepasspwd}" | openssl aes-256-cbc -a -pass pass:$tuz > ~/.ssh/salted
+echo "${keepasspwd}" | openssl aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -a -pass pass:$tuz > ~/.ssh/salted
 
--- list-pass.sh
+++ list-pass.sh
@@ -8,7 +8,8 @@ fi
 #create the salt
 tuz=`cat ~/.ssh/id_rsa | tr -dc '0-9' | head -c 12`
 #Use the salt to unlock the DB pass
-gatekeeper=`cat  ~/.ssh/salted | openssl aes-256-cbc -d -a -pass pass:$tuz`
+#gatekeeper=`cat  ~/.ssh/salted | openssl aes-256-cbc -d -a -pass pass:$tuz`
+gatekeeper=`cat  ~/.ssh/salted | openssl aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -d -a -pass pass:$tuz`
 #List the contents of the db
 echo "$tuz"
 echo "gatekeeper = $gatekeeper"
