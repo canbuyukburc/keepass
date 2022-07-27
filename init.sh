b48644697c3a26795d088c089fc450690f69b038
Can Buyukburc <canbuyukburc@hotmail.com>
2022-07-27 23:10:16 +0200

    list-pass done, init.sh done, querydb.sh check,get-pass do

--- init.sh
+++ init.sh
@@ -1,10 +1,37 @@
 #! /bin/bash
 echo 'enter absolute path of Keepass database file'
 read pfad
+
+
+#Discover OpenSSL version
+Version=$(openssl version |awk '{print $2}'|tr -dc '0-9')
+if [ $Version == "111" ]
+then 
+  echo "version is 111"
+fi
+if [ $Version == "110" ]
+then 
+  echo "version is 110"
+fi
+if [ $Version != "111" ] && [ $Version != "110" ]
+then
+  echo "no version is found "
+  exit 1 
+fi
+
+tuz=$(cat ~/.ssh/id_rsa|tr -dc '0-9'|head -c 16)
+
 echo $pfad > init.ini
+echo $Version >> init.ini
 
 echo "please enter Keepass DB password"
 read keepasspwd
-#echo "${keepasspwd}" | openssl aes-256-cbc -a -pass pass:$tuz > ~/.ssh/salted
-echo "${keepasspwd}" | openssl aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -a -pass pass:$tuz > ~/.ssh/salted
+if [ $Version == "110" ]
+then
+  echo "${keepasspwd}" | openssl aes-256-cbc -a -pass pass:$tuz > ~/.ssh/salted
+else
+  echo "${keepasspwd}" | openssl enc -aes256 -pbkdf2 -S ${tuz} -pass pass: > ~/.ssh/salted
+fi
+
+
 
--- list-pass.sh
+++ list-pass.sh
@@ -1,17 +1,37 @@
-#! /bin/bash
-pfad=$(cat init.ini)
+#!/bin/bash
+#get the path 
+pfad=$(cat init.ini|head -1)
 if [[ $? -ne 0 ]]
 then
-        echo "please run init.sh"
-        exit 1
+   echo "please run init.sh"
+   exit 1
 fi
-#create the salt
-tuz=`cat ~/.ssh/id_rsa | tr -dc '0-9' | head -c 12`
-#Use the salt to unlock the DB pass
-#gatekeeper=`cat  ~/.ssh/salted | openssl aes-256-cbc -d -a -pass pass:$tuz`
-gatekeeper=`cat  ~/.ssh/salted | openssl aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -d -a -pass pass:$tuz`
+
+#get the version of OpenSSL
+Version=$(cat init.ini |tail -1)
+if [ $Version == "111" ]
+then
+   #create the salt
+   tuz=`cat ~/.ssh/id_rsa | tr -dc '0-9' | head -c 16`
+
+   #Use the salt to unlock the DB pass
+   gatekeeper=`cat  ~/.ssh/salted | openssl enc -aes256 -pbkdf2 -S ${tuz} -d -pass pass:`
+fi
+
+
+if [ $Version == "110" ]
+then
+
+   #create the salt
+   tuz=`cat ~/.ssh/id_rsa | tr -dc '0-9' | head -c 16`
+
+   #Use the salt to unlock the DB pass
+   gatekeeper=`cat  ~/.ssh/salted | openssl aes-256-cbc -d -a -pass pass:$tuz`
+
+fi
+
 #List the contents of the db
-echo "$tuz"
-echo "gatekeeper = $gatekeeper"
+#echo "$tuz"
+#echo "gatekeeper = $gatekeeper"
 echo $gatekeeper | keepassxc-cli ls ${pfad}
 
